4311b9dc0a70ad85c03d916fc5a84c201de52495
strickler.7 <strickler.7@osu.edu>
2024-04-08 12:20:48 -0400

    Added Calendar

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,12 +1,36 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import { Helmet } from 'react-helmet'
 import { Header } from './Header.jsx'
-import { Box, Skeleton, Typography } from '@mui/material'
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Skeleton,
+  Typography
+} from '@mui/material'
 import { Await, defer, useLoaderData } from 'react-router-dom'
+import { formatDate } from '@fullcalendar/core'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list'
 
 const getData = async () => {
   await new Promise((r) => setTimeout(r, 1000))
-  return 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error eveniet fugiat in iste laboriosam neque nostrum perspiciatis similique? Architecto ducimus earum ipsam iure, minima non odit omnis porro quae rerum.'
+  return [
+    {
+      id: '1',
+      title: 'All-day event',
+      date: '2024-04-14'
+    },
+    {
+      id: '2',
+      title: 'Timed event',
+      date: '2024-04-28'
+    }
+  ]
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -14,6 +38,33 @@ export const loader = async () => defer({ data: getData() })
 
 export const Component = () => {
   let deferred = useLoaderData()
+  const [currentEvents, setCurrentEvents] = useState([])
+
+  const handleDateClick = (selected) => {
+    const title = prompt('Enter a title for your event')
+    const calendarApi = selected.view.calendar
+    calendarApi.unselect()
+
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay
+      })
+    }
+  }
+
+  const handleEventClick = (selected) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${selected.event.title}'`
+      )
+    ) {
+      selected.event.remove()
+    }
+  }
 
   return (
     <>
@@ -23,10 +74,76 @@ export const Component = () => {
       <Header title='Calendar' subtitle='Your Interactive Calendar' />
       <Box mt={3}>
         <Suspense
-          fallback={<Skeleton variant='rectangular' width='100%' height={50} />}
+          fallback={
+            <Skeleton variant='rectangular' width='100%' height={600} />
+          }
         >
           <Await resolve={deferred.data}>
-            {(data) => <Typography>{data}</Typography>}
+            {(data) => (
+              <Box display='flex' justifyContent='space-between'>
+                <Box flex='1 1 20%' backgroundColor='background.accent' p={2}>
+                  <Typography variant='h2'>Events</Typography>
+                  <List>
+                    {currentEvents.map((event) => (
+                      <ListItem
+                        key={event.id}
+                        sx={{
+                          backgroundColor: 'primary.main',
+                          color: 'primary.contrastText',
+                          mb: 2
+                        }}
+                      >
+                        <ListItemText
+                          primary={
+                            <Typography
+                              variant='h5'
+                              component='div'
+                              fontWeight='bold'
+                            >
+                              {event.title}
+                            </Typography>
+                          }
+                          secondary={
+                            <Typography variant='h5' component='div'>
+                              {formatDate(event.start, {
+                                year: 'numeric',
+                                month: 'short',
+                                day: 'numeric'
+                              })}
+                            </Typography>
+                          }
+                        />
+                      </ListItem>
+                    ))}
+                  </List>
+                </Box>
+                <Box flex='1 1 100%' ml={2}>
+                  <FullCalendar
+                    height='75vh'
+                    plugins={[
+                      dayGridPlugin,
+                      timeGridPlugin,
+                      interactionPlugin,
+                      listPlugin
+                    ]}
+                    headerToolbar={{
+                      left: 'prev,next today',
+                      center: 'title',
+                      right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+                    }}
+                    initialView='dayGridMonth'
+                    editable={true}
+                    selectable={true}
+                    selectMirror={true}
+                    dayMaxEvents={true}
+                    select={handleDateClick}
+                    eventClick={handleEventClick}
+                    eventsSet={setCurrentEvents}
+                    initialEvents={data}
+                  />
+                </Box>
+              </Box>
+            )}
           </Await>
         </Suspense>
       </Box>
